3fbf74b808da67661f8025b00b05952bd1e2230e
Sean OMeara <sean@sean.io>
2022-04-25 22:19:04 +0200

    tweaking

---
+++ scripts/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+export PLATFORM=$1
+export ISA=$2
+export VERSION=$3
+export EVENT=$4
+
+case $PLATFORM in
+    el*|fc*|amzn*)
+        export PKGFMT=rpm
+        ;;
+    *)
+        export PKGFMT=deb
+esac
+
+case $ISA in
+    armv5)
+        export ARCH=arm/v5
+        ;;
+    armv7)
+        export ARCH=arm/v7
+        ;;
+    arm64)
+        export ARCH=arm64/v8
+        ;;
+    *)
+        export ARCH=$ISA
+        ;;
+esac
+
+echo "#~~~~~~~~~~~~~~~~~~~~"
+echo "$0 variables:"
+echo "nproc: $(nproc)"
+echo "ISA: ${ISA}"
+echo "VERSION: ${VERSION}"
+echo "EVENT: ${EVENT}"
+echo "PKGFMT: ${PKGFMT}"
+echo "PWD: ${PWD}"
+echo "#~~~~~~~~~~~~~~~~~~~~"
+
+if [ ${EVENT} == "push" ]; then
+make munge_rpm bytey.spec VERSION=${VERSION}
+make munge_deb debian/changelog VERSION=${VERSION}
+fi
+
+export DOCKER_BUILDKIT=1
+docker run --privileged --rm tonistiigi/binfmt --install all
+
+docker buildx build \
+       --build-arg PLATFORM="${PLATFORM}" \
+       --platform linux/${ARCH} \
+       -f ci/Dockerfile.${PKGFMT} \
+       --target export \
+       -t build \
+       . \
+       --output .

87e3ce1031c9e7e6b43e554798b49a557892426a
Sean OMeara <sean@sean.io>
2022-05-01 20:44:41 +0200

    tweaking

--- ci/rust.sh
+++ ci/rust.sh
@@ -2,7 +2,7 @@
 
 RUST_TRIPLET=$1
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain ${RUST_TRIPLET} -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable-${RUST_TRIPLET} -y
 /root/.cargo/bin/rustup install 1.60.0
 /root/.cargo/bin/rustc --version
 /root/.cargo/bin/cargo --version

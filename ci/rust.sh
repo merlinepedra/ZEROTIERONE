ae1eeb94e8f2289b20e25d775a08d6f64863d530
Sean OMeara <sean@sean.io>
2022-05-03 16:20:04 +0200

    tweaking

--- ci/rust.sh
+++ ci/rust.sh
@@ -12,8 +12,8 @@ fi
 
 cd /tmp
 # curl --proto "=https" --tlsv1.2 -sSf "https://static.rust-lang.org/rustup/dist/${RUST_TRIPLET}/rustup-init" -O rustup-init
-echo curl -sSf "https://static.rust-lang.org/rustup/dist/${RUST_TRIPLET}/rustup-init" -O rustup-ini
-curl -sSf "https://static.rust-lang.org/rustup/dist/${RUST_TRIPLET}/rustup-init" -O rustup-init
+echo curl -sSf "https://static.rust-lang.org/rustup/dist/${RUST_TRIPLET}/rustup-init" -o rustup-init
+curl -sSf "https://static.rust-lang.org/rustup/dist/${RUST_TRIPLET}/rustup-init" -o rustup-init
 chmod +x ./rustup-init
 ./rustup-init --default-host ${RUST_TRIPLET} --default-toolchain stable -y
 

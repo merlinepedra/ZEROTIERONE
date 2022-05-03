0f265f7407c6c38980ddf7c49d7c09112b8397d2
Sean OMeara <sean@sean.io>
2022-05-03 16:09:07 +0200

    tweaking

--- ci/rust.sh
+++ ci/rust.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
-RUST_TRIPLET=$1
+if [ $# -eq 0 ]; then
+    echo "This script requires RUST_TRIPLET as an argument"
+    exit 1
+else
+    export RUST_TRIPLET=$1
+fi
 
 cd /tmp
 # curl --proto "=https" --tlsv1.2 -sSf "https://static.rust-lang.org/rustup/dist/${RUST_TRIPLET}/rustup-init" -O rustup-init
